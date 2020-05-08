eac2dad97a6744eecc8a2590d291b299f394f5ac
Sebastian Schmidt <publicarray@users.noreply.github.com>
2020-05-08 11:42:27 +1000

    code review

---
+++ src/config.rs
@@ -0,0 +1,69 @@
+use anyhow::{anyhow, bail, Context, Result};
+use bunnycdn::*;
+use serde::{Deserialize, Serialize};
+use simplelog::*;
+use std::collections::HashMap;
+use std::fs;
+use std::io;
+
+const APP_NAME: &str = "bunnycli-storage";
+
+
+pub fn get_default_config_file() -> Result<String> {
+    let mut home_dir = String::new();
+    let mut home_path = match dirs::home_dir() {
+        Some(home_path) => home_path,
+        None => bail!("Could not get Home path '~'"),
+    };
+    home_path.push(std::path::Path::new(".config/bunnycli.tml"));
+    Ok(home_path.into_os_string().into_string().unwrap())
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Config {
+    pub storage_zone: Option<StorageZone>,
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct StorageZone {
+    pub name:String,
+    pub api_endpoint: String,
+}
+
+impl Config {
+    pub fn save_config(&self, config_file: &str) -> Result<()> {
+        let toml_str = toml::to_string(self).context("Failed to convert config struct to toml string")?;
+        fs::write(config_file, toml_str).with_context(|| format!("Failed to write config file: {}", config_file))?;
+        Ok(())
+    }
+
+    pub fn storage_zone(&self) -> storage::StorageZone {
+        let keyring = keyring::Keyring::new(APP_NAME, &self.storage_zone.as_ref().unwrap().name);
+        let storage_api_key = keyring.get_password().unwrap_or_else(|err| {
+            error!("{}", err);
+            String::new()
+        });
+        storage::StorageZone::new(self.storage_zone.as_ref().unwrap().name.clone(), storage_api_key)
+    }
+
+    pub fn set_storage_zone(&self, zone_name: &str, api_key: &str) -> Result<()> {
+        let keyring = keyring::Keyring::new(APP_NAME, &zone_name);
+        keyring.set_password(&api_key)?;
+        self.storage_zone.to_owned().unwrap().name = zone_name.to_string();
+        Ok(())
+    }
+}
+
+pub fn load_config(config_file: &str) -> Result<Config> {
+    let mut config: Config = Config { storage_zone: None };
+    if std::path::Path::new(config_file).exists() {
+        let toml_str =
+            fs::read_to_string(config_file).with_context(|| format!("Failed to read config file: {}", config_file))?;
+        config = toml::from_str(&toml_str).with_context(|| format!("Failed to read config file: {}", config_file))?;
+        trace!("{:#?}", config);
+    } else {
+        return Err(anyhow!("Config file not found: {}", config_file));
+    }
+    Ok(config)
+    // settings.merge(config::Environment::with_prefix("BUNNY")).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,16 @@ extern crate bunnycdn;
 extern crate clap;
 #[macro_use]
 extern crate log;
-extern crate simplelog;
-extern crate rpassword;
 extern crate keyring;
-use anyhow::{anyhow, bail, Context, Result};
+extern crate rpassword;
+extern crate simplelog;
+use anyhow::Result;
 use bunnycdn::*;
-use clap::{App, Arg, SubCommand};
 use simplelog::*;
-use std::collections::HashMap;
-use std::fs;
-use std::io;
+pub mod config;
+use crate::config::*;
+pub mod cli;
+use crate::cli::*;
 
 use tokio::runtime::{Builder, Runtime};
 
@@ -21,105 +21,11 @@ fn rt() -> Result<Runtime> {
     Ok(Builder::new().basic_scheduler().enable_all().build()?)
 }
 
-fn get_default_config_file() -> Result<String> {
-    let mut home_dir = String::new();
-    let mut home_path = match dirs::home_dir() {
-        Some(home_path) => home_path,
-        None => bail!("Could not get Home path '~'"),
-    };
-    home_path.push(std::path::Path::new(".config/bunnycli.tml"));
-    Ok(home_path.into_os_string().into_string().unwrap())
-}
-
-use serde::{Deserialize, Serialize};
-#[derive(Debug, Deserialize, Serialize)]
-struct Config {
-    storage_zone: Option<storage::StorageZone>,
-}
-
-fn load_config(config_file: &str) -> Result<Config> {
-    let mut config: Config = Config { storage_zone: None };
-    if std::path::Path::new(config_file).exists() {
-        let toml_str =
-            fs::read_to_string(config_file).with_context(|| format!("Failed to read config file: {}", config_file))?;
-        config = toml::from_str(&toml_str).with_context(|| format!("Failed to read config file: {}", config_file))?;
-        trace!("{:#?}", config);
-    } else {
-        return Err(anyhow!("Config file not found: {}", config_file));
-    }
-    Ok(config)
-    // settings.merge(config::Environment::with_prefix("BUNNY")).unwrap();
-}
-
-fn save_config(config_file: &str, config: &Config) -> Result<()> {
-    let toml_str = toml::to_string(config).context("Failed to convert config struct to toml string")?;
-    fs::write(config_file, toml_str).with_context(|| format!("Failed to write config file: {}", config_file))?;
-    Ok(())
-}
+const APP_NAME: &str = "bunnycli-storage";
 
 fn main() -> Result<()> {
     let default_config_file = get_default_config_file()?;
-    let cli = App::new(crate_name!())
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about(crate_description!())
-        .arg(Arg::with_name("config")
-             .short("c")
-             .long("config")
-             .value_name("FILE")
-             .default_value(default_config_file.as_str())
-             .help("Sets a custom config file")
-             .takes_value(true))
-        .arg(
-            Arg::with_name("v")
-                .short("v")
-                .multiple(true)
-                .help("Sets the level of verbosity"),
-        )
-        .subcommand(
-            SubCommand::with_name("storage")
-                .about("Interact with BunnyCDN Storage Zones")
-                .arg(
-                    Arg::with_name("login")
-                        .long("login")
-                        .short("l")
-                        .value_name("STORAGE ZONE")
-                        .help("Save Login Credentials"),
-                )
-                .arg(
-                    Arg::with_name("upload")
-                        .long("upload")
-                        .short("u")
-                        .value_name("FILE")
-                        .value_name("URL")
-                        .help("Upload a file to a storage zone based on the URL path. If the directory tree does not exist, it will be created automatically."),
-                )
-                .arg(
-                    Arg::with_name("download")
-                        .long("download")
-                        .short("d")
-                        .value_name("FILE")
-                        .value_name("URL")
-                        .help("Download a file from your Storage Zone."),
-                )
-                .arg(
-                    Arg::with_name("remove")
-                        .long("remove")
-                        .short("r")
-                        .visible_alias("delete")
-                        .value_name("URL")
-                        .help("Delete an object from the storage zone. In case the object is a directory all the data in it will be recursively deleted as well.
-"),
-                )
-                .arg(
-                    Arg::with_name("directory")
-                        .long("directory")
-                        .short("i")
-                        .value_name("URL")
-                        .help("Retrieve a list of files and directories located in the given directory."),
-                ),
-        )
-        .get_matches();
+    let cli = create_cli(default_config_file.as_str());
 
     let log_level = match cli.occurrences_of("v") {
         0 => LevelFilter::Warn,
@@ -134,69 +40,52 @@ fn main() -> Result<()> {
 
     let config_file = cli.value_of("config").unwrap_or("bunnycli.toml");
     debug!("Value for config_file: {}", config_file);
-    let mut storagezone = storage::StorageZone::new(String::new(), String::new());
+
     let mut settings = load_config(&config_file)?;
-    if let Some(zone) = settings.storage_zone {
-        storagezone = zone
-    }
+    let storage_zone = settings.storage_zone();
+    trace!("The storage_zone is '{:?}'", storage_zone);
 
     let mut rt = rt()?;
 
-    if let Some(cli) = cli.subcommand_matches("storage") {
-        if cli.is_present("login") {
+    if let Some(storagecli) = cli.subcommand_matches("storage") {
+        // println!("{:?}", storagecli.subcommand_matches("login").unwrap());
+        if storagecli.is_present("login") {
             let storage_zone_name = cli.value_of("login").unwrap();
             let api_key = rpassword::read_password_from_tty(Some("Enter your Storage API Key: ")).unwrap();
 
-            let storage_zone = storage::StorageZone::new(storage_zone_name.to_string(), api_key.trim().to_string());
-            debug!("{:?}", storage_zone);
-            settings.storage_zone = Some(storage_zone);
-            save_config(config_file, &settings)?;
+            settings.set_storage_zone(storage_zone_name, api_key.trim())?;
+            settings.save_config(config_file)?;
             println!("{}", "Config Saved!");
-        // save_conf(settings)
-        // settings.merge(config::Config::try_from(&storage_zone1).unwrap());
-        // settings.refresh();
-        // settings
-        //     .set("storage.zone_name", storage_zone.to_string())
-        //     .unwrap()
-        //     .set("storage.api_key", api_key.trim().to_string())
-        //     .unwrap();
 
-        // println!("{:?}", settings.try_into::<HashMap<String, String>>().unwrap());
-        // ask for api key
-        // save storage_zone,key
-        // ~/.config/bunnycdn.toml
-        } else if cli.is_present("upload") {
-            let args: Vec<&str> = cli.values_of("upload").unwrap().collect();
+        } else if storagecli.is_present("upload") {
+            let args: Vec<&str> = storagecli.values_of("upload").unwrap().collect();
             let (file, url) = (args[0], args[1]);
             debug!("upload {} {}", file, url);
-            let response = rt.block_on(storagezone.upload_file(file, url))?;
-            trace!("cli: {:?}", response);
+            let response = rt.block_on(storage_zone.upload_file(file, url))?;
+            trace!("storagecli: {:?}", response);
             response.print();
-        } else if cli.is_present("download") {
-            let args: Vec<&str> = cli.values_of("download").unwrap().collect();
+        } else if storagecli.is_present("download") {
+            let args: Vec<&str> = storagecli.values_of("download").unwrap().collect();
             let (file, url) = (args[0], args[1]);
             debug!("download {} {}", file, url);
-            let response = rt.block_on(storagezone.download_file(file, url))?;
-            trace!("cli: {:?}", response);
+            let response = rt.block_on(storage_zone.download_file(file, url))?;
+            trace!("storagecli: {:?}", response);
             response.print();
-        } else if cli.is_present("remove") {
-            let url = cli.value_of("remove").unwrap();
+        } else if storagecli.is_present("remove") {
+            let url = storagecli.value_of("remove").unwrap();
             debug!("remove {}", url);
-            let response = rt.block_on(storagezone.delete(url))?;
-            trace!("cli: {:?}", response);
+            let response = rt.block_on(storage_zone.delete(url))?;
+            trace!("storagecli: {:?}", response);
             response.print();
-        } else if cli.is_present("directory") {
-            let url = cli.value_of("directory").unwrap();
+        } else if storagecli.is_present("directory") {
+            let url = storagecli.value_of("directory").unwrap();
             debug!("directory {}", url);
-            let response = rt.block_on(storagezone.get_objects(url))?;
-            trace!("cli: {:?}", response);
+            let response = rt.block_on(storage_zone.get_objects(url))?;
+            trace!("storagecli: {:?}", response);
             response.print();
         } else {
             error!("Missing flags");
-            println!("{}", cli.usage());
         }
-    } else {
-        println!("{}", cli.usage());
     }
     Ok(())
 }
